a575ffb2d91b148ddf4efcacef344ed165422d90
true <truee030@gmail.com>
2024-11-26 20:39:02 +0900

    feat: 사이드바 개발

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Sidebar from './components/Sidebar/Sidebar'; // Sidebar 컴포넌트 임포트
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar /> {/* Sidebar 렌더링 */}
     </div>
   );
 }
