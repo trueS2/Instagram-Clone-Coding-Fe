d87f9747ffb9a9861ab37f0b3717ddea4079c269
true <truee030@gmail.com>
2024-11-27 16:26:00 +0900

    feat: 게시글 작성 모달 개발

---
+++ src/components/PostModal/ImageDropZone.jsx
@@ -0,0 +1,12 @@
+// ImageDropZone.jsx
+import React from "react";
+
+const ImageDropZone = () => {
+    return (
+        <div className="drop-zone">
+            <p>사진과 동영상을 여기에 끌어다 놓으세요</p>
+        </div>
+    );
+};
+
+export default ImageDropZone;
---
+++ src/components/PostModal/PostDetails.jsx
@@ -0,0 +1,12 @@
+// PostDetails.jsx
+import React from "react";
+
+const PostDetails = () => {
+    return (
+        <div className="post-details">
+            <textarea placeholder="게시물 설명" maxLength="2200" />
+        </div>
+    );
+};
+
+export default PostDetails;
