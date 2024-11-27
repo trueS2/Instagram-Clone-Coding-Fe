d98c06cf7a2739ddc13418ca5b261723d2c63c2c
true <truee030@gmail.com>
2024-11-27 16:26:39 +0900

    fix: 게시글 작성 버튼에 맞춰 사이드바 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import Sidebar from './components/Sidebar/Sidebar'; // Sidebar 컴포넌트 임포트
+// App.jsx
+import React, { useState } from "react";
+import Sidebar from "./components/Sidebar/Sidebar"; // Sidebar 컴포넌트 임포트
+import UploadModal from "./components/PostModal/UploadModal"; // UploadModal 컴포넌트 임포트
+import "./index.css"; // 글로벌 스타일
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div className="App">
-      <Sidebar /> {/* Sidebar 렌더링 */}
+      <Sidebar setIsModalOpen={setIsModalOpen} /> {/* Sidebar 렌더링 */}
+      <UploadModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} /> {/* UploadModal 렌더링 */}
     </div>
   );
 }
